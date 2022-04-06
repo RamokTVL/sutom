7218563d6da1551ed4a4a36e3bebc89b0a391745
RamokTVL <sex@ramok.fr>
2022-04-06 16:54:58 +0200

    api

--- api/app.js
+++ api/app.js
@@ -4,6 +4,14 @@ const oWords = require("./data/words");
 const express = require("express");
 const app = express();
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.all("/mot.txt", (req, res) => {
     res.send(words[Math.floor(Math.random() * words.length)].toUpperCase());
 });
